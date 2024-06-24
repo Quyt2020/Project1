a0e91823356f35d1f765cc358610708989d909ba
Quyt2020 <quyet.dv214069@sis.hust.edu.vn>
2024-06-24 23:59:37 +0700

    Add files via upload

---
+++ 7segment.c
@@ -0,0 +1,23 @@
+#include<reg51.h>                                 
+
+sbit a = P3^0;
+sbit x = P3^1;
+sbit y = P3^2;
+sbit z = P3^3;
+
+void main()
+{
+    unsigned char m[10] = {0x40, 0xF9, 0x24, 0x30, 0x19, 0x12, 0x02, 0xF8, 0xE00, 0x10};
+    unsigned int i, j;
+
+    a = x = y = z = 1;
+
+    while(1)
+    {
+        for(i = 0; i < 10; i++)
+        {
+						P2 = m[i];  
+            for(j = 0; j < 32800; j++);
+        }
+    }
+}
